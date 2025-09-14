5c26da84af563463e33ddcd56cbb681848ff707d
Meraj0302 <Meraj0302@users.noreply.github.com>
2025-09-14 23:15:49 +0530

    version new

--- git.py
+++ git.py
@@ -1,1 +1,9 @@
 print("This is new testing file")
+print("git int ")
+print ("git status")
+print("git add")
+print("git commit -m ")
+print ("git restore")
+print("git remote -v")
+print("git push origin main")
+
